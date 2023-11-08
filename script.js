b1e07beb91b1f5daf5f6c21f0746d2941558a4f2
John Liter <62905216+johnliter@users.noreply.github.com>
2023-11-08 09:05:19 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scrolling to section when a TOC link is clicked
+document.querySelectorAll('nav#table-of-contents a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
